abf91a51ac556228ed634dac6453acbab1bd0a1a
danidemi <demichelis@danidemi.com>
2015-01-17 17:45:58 +0100

    build script resize images

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # You need these to build it...
 # sudo apt-get install pandoc
@@ -7,24 +7,65 @@ mkdir -p build
 
 # check grammar building a text only version
 pandoc -S -o build/cheatsheets.txt \
-	./title.yml \
-	./docker/docker_cheat_sheet.md \
-	./packer/packer.md \
-	./tomcat/tomcat_cheat_sheet.md \
-	./networking/networking_cheat_sheet.md
+	./src/title.yml \
+	./src/docker/docker_cheat_sheet.md \
+	./src/packer/packer.md \
+	./src/tomcat/tomcat_cheat_sheet.md \
+	./src/networking/networking_cheat_sheet.md
 
-cat build/cheatsheets.txt | aspell -a > build/report_aspell.txt
+#cat build/cheatsheets.txt | aspell -a > build/report_aspell.txt
 
 # converts images
 #soffice --convert-to png docker/linked_containers.odg --outdir docker/ --headless
 
+# convert all .odg to .png
+echo "convert all .odg to .png"
+find src/ -iname '*.odg' | while read odg; do
+	soffice --convert-to png ${odg} --outdir build/html --headless
+done
+
+# copy all .png
+echo "copy all .png"
+find src/ -iname '*.png' | while read png; do
+	cp ${png} build/html
+done
+
+# resize all
+echo "resize all"
+find build/ -iname '*.png' | while read png; do
+	echo "computing"
+	H=$(convert ${png} -format "%[h]" info:)
+	W=$(convert ${png} -format "%[w]" info:)
+	MAX=$(convert ${png} -format "%[fx:max(w,h)]" info:)
+	echo ${png}
+	echo "h=$H w=$W max=$MAX"
+	REDUX=$(bc <<< "scale=3; 1/($MAX/700)")
+	echo "ratio=$REDUX"
+	H=$(bc<<<"scale=0; $REDUX * $H")
+	W=$(bc<<<"scale=0; $REDUX * $W")
+	echo "h=$H w=$W"
+	convert ${png} -background white -gravity center -resize ${W}x${H} -extent ${W}x${H} ${png}
+done
 
 
 # build epub
 pandoc -S -o build/cheatsheets.epub \
-	./title.yml \
-	./docker/docker_cheat_sheet.md \
-	./packer/packer.md \
-	./tomcat/tomcat_cheat_sheet.md \
-	./networking/networking_cheat_sheet.md
+	./src/title.yml \
+	./src/docker/docker_cheat_sheet.md \
+	./src/packer/packer.md \
+	./src/tomcat/tomcat_cheat_sheet.md \
+	./src/networking/networking_cheat_sheet.md
+
+# build html
+mkdir -p build/html/
+cp style/*.css build/html/
+pandoc --standalone --smart --toc --toc-depth=3 --css=pandoc.css -o build/html/cheatsheets.html \
+	./src/title.yml \
+	./src/docker/docker_cheat_sheet.md \
+	./src/packer/packer.md \
+	./src/tomcat/tomcat_cheat_sheet.md \
+	./src/networking/networking_cheat_sheet.md
+	
+# references
+# http://www.fmwconcepts.com/imagemagick/tidbits/image.php#resize
 
